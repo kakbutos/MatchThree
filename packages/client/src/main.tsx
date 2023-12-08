8919c33ca9bb56edb2d32d57a739b4aa6781a4a7
shumilina-polina <shumilina246@gmail.com>
2023-12-08 17:18:24 +0300

    add: обратный отсчёт & роут /game

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { GamePage } from './pages/game';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="game" element={<GamePage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
