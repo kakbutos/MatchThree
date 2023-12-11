f5eec38955726ad180f03312cce29093b91cd641
belyshevaoksanai <belyshevaoksanai@yandex.ru>
2023-12-11 14:37:30 +0300

    COD-17: Добавлен редирект при 401 ошибке

--- packages/client/src/hooks/useApiCall.ts
+++ packages/client/src/hooks/useApiCall.ts
@@ -1,5 +1,7 @@
 import { AxiosError, AxiosResponse } from 'axios';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getRouteLogin } from '../constants/router/router';
 
 interface ResponseError {
   reason: string;
@@ -8,18 +10,28 @@ interface ResponseError {
 export const useApiCall = <F extends (body: any) => ReturnType<F>>(
   apiCall: F
 ) => {
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
 
   const call = async (body: Parameters<F>) => {
-    let res: ReturnType<F> | ResponseError | undefined;
+    let res: ReturnType<F> | AxiosResponse<ResponseError> | undefined;
     try {
       setIsLoading(true);
 
       const response = await apiCall(body);
       res = (response as AxiosResponse<ReturnType<F>>).data;
     } catch (error) {
-      res = (error as AxiosError<ResponseError>).response?.data;
       console.error(error);
+      if (error instanceof AxiosError) {
+        if (
+          error.response?.status === 401 &&
+          window.location.pathname !== getRouteLogin()
+        ) {
+          navigate(getRouteLogin());
+        } else {
+          res = error.response;
+        }
+      }
     } finally {
       setIsLoading(false);
     }
