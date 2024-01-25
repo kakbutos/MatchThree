e92050b47b9944a5be20421d2b052598fbca450e
belyshevaoksanai <belyshevaoksanai@yandex.ru>
2024-01-25 22:37:28 +0300

    fix

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -3,19 +3,15 @@ import App from './src/app';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-
-export const store = configureStore({
-  reducer: {},
-});
+import { store } from './src/store';
 
 // TODO COD-55 добавить провайдер Redux и Router, изменить путь к настоящему App
 export function render(url: string) {
   return renderToString(
-    <Provider store={store}>
-      <StaticRouter location={url}>
+    <StaticRouter location={url}>
+      <Provider store={store}>
         <App />
-      </StaticRouter>
-    </Provider>
+      </Provider>
+    </StaticRouter>
   );
 }
