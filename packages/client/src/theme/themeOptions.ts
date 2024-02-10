b2d5ca334ef4ee03429912e5d514c6c36ca9a27e
belyshevaoksanai <belyshevaoksanai@yandex.ru>
2024-02-10 07:56:01 +0300

    COD-62: Добавлена темная тема.

--- packages/client/src/theme/themeOptions.ts
+++ packages/client/src/theme/themeOptions.ts
@@ -23,6 +23,32 @@ export const themeOptions: ThemeOptions = {
       secondary: palette.grey,
       disabled: palette.greyLight,
     },
+    background: {
+      default: palette.white,
+      paper: primary[500],
+    },
+  },
+  components,
+};
+
+export const themeOptionsDark: ThemeOptions = {
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: primary[500],
+      light: primary[50],
+      dark: primary[900],
+    },
+    secondary: {
+      main: secondary[700],
+      dark: secondary[900],
+      light: secondary[50],
+      contrastText: palette.white,
+    },
+    background: {
+      default: palette.black,
+      paper: palette.black,
+    },
   },
   components,
 };
