8938374d2c9fcd6d02739518b047e235b4e21c01
Egor Shashurin <egor.sh.2002@yandex.ru>
2024-02-18 16:34:39 +0300

    Добавлен функционал реакции эмодзи на сообщения и ответы на сообщения

--- packages/client/src/widgets/comment/Comment.tsx
+++ packages/client/src/widgets/comment/Comment.tsx
@@ -1,11 +1,27 @@
-import { Box, Typography, Button, Avatar } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Button,
+  Avatar,
+  IconButton,
+  Popover,
+} from '@mui/material';
 import ReplyIcon from '@/assets/icons/reply.svg?react';
 import styles from './comment.module.scss';
 import { ReplyCommentForm } from './ReplyCommentForm';
-import { CommentResponse, ReplyResponse } from '@/types/forum/api';
-import { FC, useMemo } from 'react';
+import {
+  CommentResponse,
+  ReactionData,
+  ReplyResponse,
+} from '@/types/forum/api';
+import { FC, useMemo, useState } from 'react';
 import moment from 'moment';
 import { Reply } from './Reply';
+import SentimentSatisfiedOutlined from '@mui/icons-material/SentimentSatisfiedOutlined';
+import Picker from '@emoji-mart/react';
+import data from '@emoji-mart/data';
+import { useAppSelector } from '@/hooks/useAppSelector';
+import { UserStore } from '@/store/user';
 
 const sortByDate = (a: ReplyResponse, b: ReplyResponse) =>
   a.createdAt < b.createdAt ? 1 : -1;
@@ -23,40 +39,179 @@ export const Comment: FC<CommentProps> = ({
   onOpenReply,
   fetchTopic,
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [localComment, setLocalComment] = useState<CommentResponse>(comment);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [showPicker, setShowPicker] = useState(false);
+  const toggleEmojiPicker = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(showPicker ? null : event.currentTarget);
+    setShowPicker(value => !value);
+    if (isHovered) {
+      setIsHovered(prev => !prev);
+    }
+  };
+  const currentUser = useAppSelector(UserStore.selectors.selectCurrentUser);
+
+  const handleEmojiSelect = ({ native }: { native: string }) => {
+    addNewReaction(native);
+    setAnchorEl(null);
+    setShowPicker(value => !value);
+    setIsHovered(false);
+  };
+
+  const updateReactions = (newReactions: Array<ReactionData>): void => {
+    setLocalComment(prevComment => ({
+      ...prevComment,
+      reactions: newReactions,
+    }));
+  };
+
+  const addNewReaction = (reaction: string): void => {
+    let indexIfAlreadyContains = null;
+    localComment?.reactions?.map((item, index) => {
+      if (item.reaction !== reaction) return;
+
+      indexIfAlreadyContains = index;
+    });
+
+    if (indexIfAlreadyContains !== null) {
+      if (checkCurrentUserExistInReactions(indexIfAlreadyContains)) {
+        deleteReaction(indexIfAlreadyContains);
+        return;
+      }
+
+      const newReactions = [...localComment.reactions];
+      newReactions[indexIfAlreadyContains] = {
+        ...newReactions[indexIfAlreadyContains],
+        users: newReactions[indexIfAlreadyContains].users.concat(
+          currentUser.id
+        ),
+      };
+      updateReactions(newReactions);
+      return;
+    }
+
+    updateReactions(
+      localComment.reactions.concat({
+        reaction: reaction,
+        users: [currentUser.id],
+      })
+    );
+  };
+
+  const handleReactionClick = (index: number): void => {
+    const indexIfAlreadyContainsInUser = localComment.reactions[
+      index
+    ].users.indexOf(currentUser.id);
+
+    if (indexIfAlreadyContainsInUser === -1) {
+      addNewReaction(localComment.reactions[index].reaction);
+      return;
+    }
+
+    deleteReaction(index);
+  };
+
+  const deleteReaction = (index: number): void => {
+    const indexIfAlreadyContainsInUser = localComment.reactions[
+      index
+    ].users.indexOf(currentUser.id);
+
+    const newReactions = [...localComment.reactions];
+    const newUsers = [...newReactions[index].users];
+    newUsers.splice(indexIfAlreadyContainsInUser, 1);
+    newReactions[index] = {
+      ...newReactions[index],
+      users: newUsers,
+    };
+
+    if (!newReactions[index].users.length) {
+      newReactions.splice(index, 1);
+    }
+
+    updateReactions(newReactions);
+  };
+
+  const checkCurrentUserExistInReactions = (index: number): boolean => {
+    return localComment.reactions[index].users.indexOf(currentUser?.id) !== -1;
+  };
+
   const sortedReplies = useMemo(
     () => comment.replies?.sort(sortByDate),
     [comment]
   );
   return (
     <>
-      <Box className={styles.commentContainer}>
-        <Box display="flex" gap="16px">
-          <Avatar
-            alt="avatar"
-            src={`https://ui-avatars.com/api/?name=${comment.userId}`}
-          />
-          <Box className={styles.commentInfo}>
-            <Typography component="span">
-              Пользователь {comment.userId}
-            </Typography>
-            <Typography component="span">{comment.content}</Typography>
+      <Box
+        className={styles.wrapper}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}>
+        <Box className={styles.commentContainer}>
+          <Box display="flex" gap="16px">
+            <Avatar
+              alt="avatar"
+              src={`https://ui-avatars.com/api/?name=${localComment.userId}`}
+            />
+            <Box className={styles.commentInfo}>
+              <Box display={'flex'}>
+                <Typography component="span">
+                  Пользователь {localComment.userId}
+                </Typography>
+                {isHovered && (
+                  <div className={styles.smileyIcon}>
+                    <IconButton
+                      aria-label="toggle emoji popover"
+                      onClick={toggleEmojiPicker}
+                      onMouseDown={toggleEmojiPicker}>
+                      <SentimentSatisfiedOutlined style={{ color: 'white' }} />
+                    </IconButton>
+                    <Popover
+                      open={showPicker}
+                      onClose={toggleEmojiPicker}
+                      anchorEl={anchorEl}>
+                      <Picker data={data} onEmojiSelect={handleEmojiSelect} />
+                    </Popover>
+                  </div>
+                )}
+              </Box>
+              <Typography component="span">{localComment.content}</Typography>
+            </Box>
+          </Box>
+          <Box className={styles.commentInfo} alignItems="end">
+            <Typography component="span">{`${moment(
+              localComment.createdAt
+            ).format('MMM Do YY, h:mm a')}`}</Typography>
+            <Button
+              variant="text"
+              color="secondary"
+              endIcon={<ReplyIcon />}
+              onClick={() => onOpenReply(localComment.id)}>
+              Ответить
+            </Button>
           </Box>
         </Box>
-        <Box className={styles.commentInfo} alignItems="end">
-          <Typography component="span">{`${moment(comment.createdAt).format(
-            'MMM Do YY, h:mm a'
-          )}`}</Typography>
-          <Button
-            variant="text"
-            color="secondary"
-            endIcon={<ReplyIcon />}
-            onClick={() => onOpenReply(comment.id)}>
-            Ответить
-          </Button>
+        <Box className={styles.reactionBox}>
+          {localComment.reactions.length
+            ? localComment.reactions.map((reaction, index) => (
+                <Box
+                  className={`${styles.reaction} ${
+                    checkCurrentUserExistInReactions(index)
+                      ? styles.reactionActive
+                      : ''
+                  }`}
+                  onClick={() => handleReactionClick(index)}
+                  key={index}>
+                  <Typography component="span">{reaction.reaction}</Typography>
+                  <Typography component="span">
+                    {reaction.users.length}
+                  </Typography>
+                </Box>
+              ))
+            : ''}
         </Box>
       </Box>
-      {openedId === comment.id && (
-        <ReplyCommentForm fetchTopic={fetchTopic} commentId={comment.id} />
+      {openedId === localComment.id && (
+        <ReplyCommentForm fetchTopic={fetchTopic} commentId={localComment.id} />
       )}
       {sortedReplies.map(reply => (
         <Reply key={reply.id} reply={reply} />
--- packages/client/src/widgets/comment/Reply.tsx
+++ packages/client/src/widgets/comment/Reply.tsx
@@ -1,35 +1,177 @@
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, Typography, IconButton, Popover } from '@mui/material';
 import styles from './comment.module.scss';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import moment from 'moment';
-import { ReplyResponse } from '@/types/forum/api';
+import { ReactionData, ReplyResponse } from '@/types/forum/api';
+import SentimentSatisfiedOutlined from '@mui/icons-material/SentimentSatisfiedOutlined';
+import Picker from '@emoji-mart/react';
+import data from '@emoji-mart/data';
+import { useAppSelector } from '@/hooks/useAppSelector';
+import { UserStore } from '@/store/user';
 
 interface ReplyProps {
   reply: ReplyResponse;
 }
 
 export const Reply: FC<ReplyProps> = ({ reply }) => {
+  const [localReply, setLocalReply] = useState<ReplyResponse>(reply);
+  const [isHovered, setIsHovered] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [showPicker, setShowPicker] = useState(false);
+  const toggleEmojiPicker = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(showPicker ? null : event.currentTarget);
+    setShowPicker(value => !value);
+    if (isHovered) {
+      setIsHovered(prev => !prev);
+    }
+  };
+  const currentUser = useAppSelector(UserStore.selectors.selectCurrentUser);
+
+  const handleEmojiSelect = ({ native }: { native: string }) => {
+    addNewReaction(native);
+    setAnchorEl(null);
+    setShowPicker(value => !value);
+    setIsHovered(false);
+  };
+
+  const updateReactions = (newReactions: Array<ReactionData>): void => {
+    setLocalReply(prevComment => ({
+      ...prevComment,
+      reactions: newReactions,
+    }));
+  };
+
+  const addNewReaction = (reaction: string): void => {
+    let indexIfAlreadyContains = null;
+    localReply?.reactions?.map((item, index) => {
+      if (item.reaction !== reaction) return;
+
+      indexIfAlreadyContains = index;
+    });
+
+    if (indexIfAlreadyContains !== null) {
+      if (checkCurrentUserExistInReactions(indexIfAlreadyContains)) {
+        deleteReaction(indexIfAlreadyContains);
+        return;
+      }
+
+      const newReactions = [...localReply.reactions];
+      newReactions[indexIfAlreadyContains] = {
+        ...newReactions[indexIfAlreadyContains],
+        users: newReactions[indexIfAlreadyContains].users.concat(
+          currentUser.id
+        ),
+      };
+      updateReactions(newReactions);
+      return;
+    }
+
+    updateReactions(
+      localReply.reactions.concat({
+        reaction: reaction,
+        users: [currentUser.id],
+      })
+    );
+  };
+
+  const handleReactionClick = (index: number): void => {
+    const indexIfAlreadyContainsInUser = localReply.reactions[
+      index
+    ].users.indexOf(currentUser.id);
+
+    if (indexIfAlreadyContainsInUser === -1) {
+      addNewReaction(localReply.reactions[index].reaction);
+      return;
+    }
+
+    deleteReaction(index);
+  };
+
+  const deleteReaction = (index: number): void => {
+    const indexIfAlreadyContainsInUser = localReply.reactions[
+      index
+    ].users.indexOf(currentUser.id);
+
+    const newReactions = [...localReply.reactions];
+    const newUsers = [...newReactions[index].users];
+    newUsers.splice(indexIfAlreadyContainsInUser, 1);
+    newReactions[index] = {
+      ...newReactions[index],
+      users: newUsers,
+    };
+
+    if (!newReactions[index].users.length) {
+      newReactions.splice(index, 1);
+    }
+
+    updateReactions(newReactions);
+  };
+
+  const checkCurrentUserExistInReactions = (index: number): boolean => {
+    return localReply.reactions[index].users.indexOf(currentUser?.id) !== -1;
+  };
+
   return (
-    <>
+    <Box
+      className={styles.wrapper}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}>
       <Box className={styles.replyContainer}>
         <Box display="flex" gap="6px">
           <Avatar
             alt="avatar"
-            src={`https://ui-avatars.com/api/?name=${reply.userId}`}
+            src={`https://ui-avatars.com/api/?name=${localReply.userId}`}
           />
           <Box className={styles.commentInfo}>
-            <Typography component="span">
-              Пользователь {reply.userId}
-            </Typography>
-            <Typography component="span">{reply.content}</Typography>
+            <Box display={'flex'}>
+              <Typography component="span" padding={'0 0 10px 0'}>
+                Пользователь {localReply.userId}
+              </Typography>
+              {isHovered && (
+                <div className={styles.smileyReplyIcon}>
+                  <IconButton
+                    aria-label="toggle emoji popover"
+                    onClick={toggleEmojiPicker}
+                    onMouseDown={toggleEmojiPicker}>
+                    <SentimentSatisfiedOutlined style={{ color: 'white' }} />
+                  </IconButton>
+                  <Popover
+                    open={showPicker}
+                    onClose={toggleEmojiPicker}
+                    anchorEl={anchorEl}>
+                    <Picker data={data} onEmojiSelect={handleEmojiSelect} />
+                  </Popover>
+                </div>
+              )}
+            </Box>
+            <Typography component="span">{localReply.content}</Typography>
           </Box>
         </Box>
         <Box className={styles.commentInfo} alignItems="end">
-          <Typography component="span">{`${moment(reply.createdAt).format(
+          <Typography component="span">{`${moment(localReply.createdAt).format(
             'MMM Do YY, h:mm a'
           )}`}</Typography>
         </Box>
       </Box>
-    </>
+      <Box className={styles.reactionBox}>
+        {localReply.reactions.length
+          ? localReply.reactions.map((reaction, index) => (
+              <Box
+                className={`${styles.reaction} ${
+                  checkCurrentUserExistInReactions(index)
+                    ? styles.reactionActive
+                    : ''
+                }`}
+                onClick={() => handleReactionClick(index)}
+                key={index}>
+                <Typography component="span">{reaction.reaction}</Typography>
+                <Typography component="span">
+                  {reaction.users.length}
+                </Typography>
+              </Box>
+            ))
+          : ''}
+      </Box>
+    </Box>
   );
 };
